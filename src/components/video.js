c5f8c8b6f9c921c94b26f7f7ae51e2c4206f90ca
nabendu82 <nabendu.biswas@gmail.com>
2020-04-13 23:23:49 +0530

    part-7 blog

--- src/components/video.js
+++ src/components/video.js
@@ -1,18 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import TwilioVideo from "twilio-video"
 
 const Video = ({ token }) => {
+    const localVidRef = useRef()
+    const remoteVidRef = useRef()
+
     useEffect(() => {
         TwilioVideo.connect(token, { video: true, audio: true, name: "SVA" }).then(
             result => {
-                console.log("Successfully joined room", result);
+                // Attach the local video
+                TwilioVideo.createLocalVideoTrack().then(track => {
+                    localVidRef.current.appendChild(track.attach())
+                })
+                //Attach video for all remote participants
+                const addParticipant = participant => {
+                    participant.tracks.forEach(publication => {
+                        if (publication.isSubscribed) {
+                            const track = publication.track
+                            remoteVidRef.current.appendChild(track.attach())
+                        }
+                    })
+                }
+                result.participants.forEach(addParticipant)
             })
-
-    },[token])
+    }, [token])
 
     return (
         <div>
-            Video
+            <div ref={localVidRef} />
+            <div ref={remoteVidRef} />
         </div>
     )
 }
